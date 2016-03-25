f7edf27699790871aa6a57a459ab2009de32811c
tannerjaime <tannj583@newschool.edu>
2016-03-24 21:27:16 -0700

    refined

--- BeatsByDavid/sketch.js
+++ BeatsByDavid/sketch.js
@@ -2,6 +2,8 @@ function preload() {
   mySound = loadSound('Boop.m4a');
   Sound2 = loadSound('TiTiKuh.m4a');
   Sound3 = loadSound('OOGH.m4a');
+  Sound4 = loadSound('BanahBanah.m4a');
+  Sound5 = loadSound('Damn son.m4a');
 }
 
 function setup() {
@@ -10,47 +12,72 @@ function setup() {
 
 //boop
   boop = createButton('BOOP');
-  boop.position(width * 0.5, height * 0.25);
+  boop.class('myButton');
+  boop.position(0, height * 0.05);
   boop.id("boop");
 
   $('#boop').click(function() {
     mySound.setVolume(0.2);
     mySound.play();
   });
-  $('#boop').on('click touchstart', function() {
-    mySound.setVolume(0.2);
-    mySound.play();
-  });
+  // $('#boop').on('click touchstart', function() {
+  //   mySound.setVolume(0.2);
+  //   mySound.play();
+  // });
 
 //ti ti kuh
-  titikuh = createButton('TT KUH');
-  titikuh.position(width * 0.5, height * 0.5);
+  titikuh = createButton('TTK');
+  titikuh.position(0, height * 0.25);
+  titikuh.class('myButton');
   titikuh.id("titikuh");
 
   $('#titikuh').click(function() {
     Sound2.setVolume(0.2);
     Sound2.play();
   });
-  $('#titikuh').on('click touchstart', function() {
-    Sound2.setVolume(0.2);
-    Sound2.play();
-  });
+  // $('#titikuh').on('click touchstart', function() {
+  //   Sound2.setVolume(0.2);
+  //   Sound2.play();
+  // });
 
 //OOGH
   OOGH = createButton('OOGH');
-  OOGH.position(width * 0.5, height * 0.75);
+  OOGH.position(0, height * 0.65);
+  OOGH.class('myButton');
   OOGH.id("oogh");
   
     $('#oogh').click(function() {
     Sound3.setVolume(0.2);
     Sound3.play();
   });
-  $('#oogh').on('click touchstart', function() {
-    Sound3.setVolume(0.2);
-    Sound3.play();
+  // $('#oogh').on('click touchstart', function() {
+  //   Sound3.setVolume(0.2);
+  //   Sound3.play();
+  // });
+
+//banah banah
+
+  banah= createButton('BNABNA');
+  banah.position(0, height * 0.45);
+  banah.class('myButton');
+  banah.id("bana");
+  
+    $('#bana').click(function() {
+    Sound4.setVolume(0.4);
+    Sound4.play();
   });
+  
+//damn son
 
-
+  damn = createButton('DAMNSON');
+  damn.position(0, height * 0.85);
+  damn.class('myButton');
+  damn.id("damn");
+  
+    $('#damn').click(function() {
+    Sound5.setVolume(0.2);
+    Sound5.play();
+  });
 }
 
 function draw() {
